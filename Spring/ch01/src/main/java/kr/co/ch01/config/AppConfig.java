afbffc85306e789c3c3629f4f05c4357dd9e291a
whddls0323 <whddls0323@naver.com>
2025-09-15 11:04:52 +0900

    add spring

---
+++ Spring/ch01/src/main/java/kr/co/ch01/config/AppConfig.java
@@ -0,0 +1,24 @@
+package kr.co.ch01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"kr.co.ch01"})
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views", ".jsp");
+    }
+
+    @Bean
+    public String test() {
+        return "hello";
+    }
+}
